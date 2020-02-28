f011fd40256790005bb24b151eee3cef58e447aa
Peter Levinsky <pele@easj.dk>
2020-02-28 11:04:22 +0100

    REST consumer tilføjet

---
+++ KageApp/KagerWorker.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using BagerLib.model;
+using Newtonsoft.Json;
+
+namespace KageApp
+{
+    internal class KagerWorker
+    {
+        private string URI = "http://localhost:50915/api/Kager/";
+
+        public async void Start()
+        {
+            IList<Kage> kager = await HentAlleKager();
+
+            foreach (Kage kage in kager)
+            {
+                Console.WriteLine(kage);
+            }
+
+            Kage k = new Kage("rumkugle", 45, 5);
+            bool erOprettet = await OpretKager(k);
+            Console.WriteLine("Rumkugle er oprettet " + erOprettet);
+        }
+
+        private async Task<IList<Kage>> HentAlleKager()
+        {
+            List<Kage> kager;
+
+            using (HttpClient client = new HttpClient())
+            {
+                //HttpResponseMessage res = await client.GetAsync(URI);
+                String json = await client.GetStringAsync(URI);
+                kager = JsonConvert.DeserializeObject<List<Kage>>(json);
+            }
+
+            return kager;
+        }
+
+        private async Task<bool> OpretKager(Kage kage)
+        {
+            bool ok = false;
+
+            using (HttpClient client = new HttpClient())
+            {
+                // lave Body
+                String jString = JsonConvert.SerializeObject(kage);
+                StringContent content = new StringContent(jString, Encoding.UTF8, "application/json");
+                HttpResponseMessage result  = await client.PostAsync(URI, content);
+
+                if (result.IsSuccessStatusCode)
+                {
+                    string okRes = await result.Content.ReadAsStringAsync();
+                    ok = JsonConvert.DeserializeObject<bool>(okRes);
+                }
+            }
+
+            return ok;
+
+        }
+
+    }
+}
---
+++ KageApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KagerWorker worker = new KagerWorker();
+            worker.Start();
+
+
+            Console.ReadLine();
+        }
+    }
+}
